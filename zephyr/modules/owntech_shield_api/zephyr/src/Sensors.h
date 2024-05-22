b5d93cb5a4371ba1560df693ee7f23a9a599b8cb
Clément Foucher <clement.foucher@laas.fr>
2024-05-22 14:52:20 +0200

    Harmonize headers SPDX Identifiers.

--- zephyr/modules/owntech_shield_api/zephyr/src/Sensors.h
+++ zephyr/modules/owntech_shield_api/zephyr/src/Sensors.h
@@ -14,7 +14,7 @@
  *   You should have received a copy of the GNU Lesser General Public License
  *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * SPDX-License-Identifier: LGPL-V2.1
+ * SPDX-License-Identifier: LGPL-2.1
  */
 
 /**
